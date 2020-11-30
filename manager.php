85005a9ee0af166c97303dda13de274842bc42d0
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-30 22:43:26 +0200

    Update manager.php

--- manager.php
+++ manager.php
@@ -8,9 +8,10 @@
 <td aling="right"><a href="form1.html">الصفحة الرئيسية</a></td>
 </tr>
 </table>
-<form action="form6.html"method"="post">
+<form action="form6.php"method"="post">
 <h1 align="center"><font color="0000"><font size="5">username<input type="text"name="ftooh"><br></font></h1>
 <h1 align="center"><font color="0000"><font size="5">password<input type="pass"name="221"><br></font></h1>
 <h1 align="center"><font color="0000"><font size="5">login<input type="submit"name="login"><marquee direction="up"scrollamount="6"><br><br><fontsize="20"><fontcolor="yellow">wellcome</fontc></fonts><br><br></marquee><br><p><br></font</h1>
-<h1 aling="right"><a href="alfa2i72222@gmail.com">الانتقال الي الايميل</a></h1>
-</body></html>
+<h1 aling="right"><index.php>الانتقال الي الايميل</a></h1>
+</body
+                 ></html>
