b4138d9c117d6c3dc5e6c029a980b6e1e37d3016
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-30 22:50:41 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,60 @@
-﻿<html>
-<body>
-<img src="tt2.jpg"width="150"hight="230"style="float:right;"><img src="dd.jpg"width="150"hight="230">
-<body style="background-image:url('bb.jpg')">
-<form action="form1.php"method"="post">
-username<input type="text"name="ftooh"><br>
-password<input type="password"name="221"><br>
-login<input type="submit"name="login"><marquee direction="up"scrollamount="6"><br><br><fontsize="20"><fontcolor="yellow">wellcome</fontc></fonts><br><br></marquee><br><p><br
-<h1 aling="right"><a href="form4.php">الانتقال الي الايميل</a></h1>
+<center>
+    <a href="index.php">الصفحه الرئيسيه</a>
+
+</center>
+<br>
+<br>
+
+<center>
+<form method="post">
+    username : <input type="text" name="username" required />
+    <br>
+   password : <input type="password" name="password" required />
+<br>
+<button name="login" type="submit">login</button>
+
+</form>
+
+
+</center>
+
+
+
+<?php
+$ussename="basheer";
+$password="12345";
+
+if(isset($_POST['login'])){
+
+    $getUserName=$_POST['username'];
+    $getpassword=$_POST['password'];
+
+    if($ussename=== $getUserName && $password === $password){
+session_start();
+$_SESSION['USER'] = $getUserName;
+$_SESSION['PASSWORD'] = $getpassword;
+$_SESSION['LOGIN'] =true;
+
+  header("location: form2.php");
+    }else{
+        echo"<center>";
+        echo"خطأ في اسم المستخدم او كلمه المرور";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
 </body>
 </html>
